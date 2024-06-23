f80c7d5d4d4b556bf388f1c0d33a3c29d361abcb
S.Umar <shumarb@outlook.com>
2024-06-23 12:22:33 +0800

    Updated files to reflect change from BookStore to Bookzone

---
+++ src/test/java/com/bookzone/BookzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
