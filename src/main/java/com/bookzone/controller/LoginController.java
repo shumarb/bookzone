185eea74ba4e267b923e994189a68b2f1dc379b2
S.Umar <shumarb@outlook.com>
2024-08-15 17:27:03 +0800

    Updated comments, changed name of logger

--- src/main/java/com/bookzone/controller/LoginController.java
+++ src/main/java/com/bookzone/controller/LoginController.java
@@ -23,31 +23,32 @@ public class LoginController {
     /**
      * Logger to monitor operational flow and facilitate troubleshooting.
      */
-    private static final Logger loginControllerLogger = LogManager.getLogger(LoginController.class);
+    private static final Logger logger = LogManager.getLogger(LoginController.class);
 
     @Autowired
     private LoginService loginService;
 
     /**
-     * Goes to the Login page.
+     * Handles GET mapping related to displaying the Login page.
      *
-     * @param httpSession The HttpSession object.
-     * @return name of view for the Login page.
+     * @param   httpSession The HttpSession object.
+     * @return  The name of view for the Login page.
      */
     @GetMapping("/login")
     public String showLogin(HttpSession httpSession) {
-        loginControllerLogger.info("LoginControllerLogger: Currently at Login page.");
+        logger.info("Currently at Login page.");
         return "login";
     }
 
     /**
-     * Manages a Librarian's login process,
+     * Handles the POST mapping related to processing login attempts.
      *
-     * @param email     The email address of the {@link Person} entity attempting to log in.
-     * @param password  The password of the {@link Person} entity attempting to log in.
-     * @param model     The model where attributes can be added for the view
-     * @return          Redirection to the Home page for a successful login, or back to the login page
-     *                  with an error message displayed for unsuccessful login
+     * @param email         The email address of the user.
+     * @param password      The password of the user.
+     * @param httpSession   The HttpSession to set the logged-in user.
+     * @param model         The model where attributes can be added for the view
+     * @return              Redirection to the Home page for a successful login, or login page
+     *                      with an message explaining reason for unsuccessful login.
      */
     @PostMapping("/login")
     public String login(@RequestParam String email,
@@ -57,16 +58,16 @@ public class LoginController {
         try {
             Person loggedInPerson = loginService.login(email, password);
             httpSession.setAttribute("loggedInPerson", loggedInPerson);
-            loginControllerLogger.info("LoginControllerLogger: Successful login. Proceeding to Home page");
+            logger.info("LoginControllerLogger: Successful login. Proceeding to Home page");
             return "redirect:/home";
 
         } catch (UnsuccessfulLoginException e) {
-            loginControllerLogger.error("LoginControllerLogger: Unsuccessful login. Proceeding to Login page with error message displayed.");
+            logger.error("LoginControllerLogger: Unsuccessful login. Proceeding to Login page with error message displayed.");
             model.addAttribute("error", "Invalid email or password. Please try again.");
             return "login";
 
         } catch (Exception e) {
-            loginControllerLogger.error("Unsuccessful login. Proceeding to Login page with error message displayed.");
+            logger.error("Unsuccessful login. Proceeding to Login page with error message displayed.");
             model.addAttribute("error", "Unexpected error occurred. Please try again later.");
             return "login";
         }
