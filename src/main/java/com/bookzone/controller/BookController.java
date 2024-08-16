792e919e566fc514c92fb65a567d4e84dfc5c42e
S.Umar <shumarb@outlook.com>
2024-08-16 16:31:31 +0800

    Updated mapping request and javadoc comments

--- src/main/java/com/bookzone/controller/BookController.java
+++ src/main/java/com/bookzone/controller/BookController.java
@@ -1,7 +1,5 @@
 /**
- * The BookController class handles incoming requests, processes these requests, 
- * and provides the appropriate responses for requests on adding, editing, and deleting 
- * a book from the BookZone catalogue.
+ * Handles requests for adding, editing, and deleting books.
  * 
  * @author Sheikh Umar
  */
@@ -14,11 +12,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bookzone.model.Book;
@@ -33,15 +27,15 @@ public class BookController {
 	private static final Logger logger = LogManager.getLogger(BookController.class);
 	
 	/**
-	 * BookService for accessing and managing the Book entities
+	 * Service for managing {@link Book} entities.
 	 */
 	@Autowired
 	private BookService bookService;
 	
 	/**
-	 * Shows the Add Book page.
+	 * Handles the GET request for the Add Book page.
 	 * 
-	 * @return name of Add page.
+	 * @return View name of the Add page.
 	 */
 	@GetMapping("/add")
 	public String showAdd() {
@@ -50,10 +44,9 @@ public class BookController {
 	}
 	
 	/**
-	 * Gets all books in the catalogue.
+	 * Handles the GET request to retrieve all books in the catalogue.
 	 * 
-	 * @return 	A ModelAndView object containing the view name of the Catalogue page,
-	 * 			a model attribute "book", and a list of books retrieved from the the book service.
+	 * @return 	A ModelAndView object containing the view name of the Catalogue page and list of books.
 	 */
 	@GetMapping("/catalogue")
 	public ModelAndView getAllBooks() {
@@ -63,10 +56,10 @@ public class BookController {
 	}
 	
 	/**
-	 * Adds a Book to the catalogue.
+	 * Handles the POST request to add a new {@link Book} to the catalogue.
 	 *
-	 * @param book: Book object to add to catalogue.
-	 * @return A redirect to the Catalogue page.
+	 * @param book 	The {@link Book} entity to add.
+	 * @return 		Redirection to view name of Catalogue page.
 	 */
 	@PostMapping("/save")
 	public String addBook(@ModelAttribute Book book) {
@@ -76,13 +69,13 @@ public class BookController {
 	}
 	
 	/**
-	 * Retrieves a Book to edit by id.
+	 * Handles the PUT request to update a {@link Book} entity.
 	 * 
-	 * @param id Id of the Book to edit.
-	 * @param model The model that the Book will be added.
-	 * @return The view name of the edit page.
+	 * @param id 		The id of the {@link Book} entity to edit.
+	 * @param model 	The model to add the {@link Book} entity to.
+	 * @return 			The view name of the Edit page.
 	 */
-	@RequestMapping("/editBook/{id}")
+	@PutMapping("/editBook/{id}")
 	public String editBook(@PathVariable("id") long id, Model model) {
 		Book book = bookService.getBookById(id);
 		logger.info("Editing book: {}", book.toString());
@@ -91,10 +84,10 @@ public class BookController {
 	}
 	
 	/**
-	 * Deletes a Book from the catalogue by its id, as well as the SpecialBook list if it exists.
+	 * Handles the DELETE request to remove a {@link Book} entity from the catalogue.
 	 * 
-	 * @param id 	Id of Book to be deleted.
-	 * @return 		Redirection to the view name of the catalogue page.
+	 * @param id 	The id of a {@link Book} entity to be deleted.
+	 * @return 		Redirection to the view name of the Catalogue page.
 	 */
 	@RequestMapping("/deleteBook/{id}")
 	public String deleteBook(@PathVariable("id") long id) {
