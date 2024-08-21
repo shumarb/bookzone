6dd4968624e85fdadcb5d9cd788c08f472b1466f
S.Umar <shumarb@outlook.com>
2024-08-21 10:40:33 +0800

    Added unit test for successful login

--- src/test/java/com/bookzone/controller/LoginControllerTest.java
+++ src/test/java/com/bookzone/controller/LoginControllerTest.java
@@ -1,25 +1,47 @@
 package com.bookzone.controller;
 
+import com.bookzone.exceptions.UnsuccessfulLoginException;
+import com.bookzone.model.Librarian;
+import com.bookzone.model.Person;
+import com.bookzone.service.LoginService;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class LoginControllerTest {
 
     @InjectMocks
     private LoginController loginController;
 
+    @Mock
+    private HttpSession httpSession;
+
+    @Mock
+    private LoginService loginService;
+
+    @Mock
+    private Model model;
+
+    @Mock
+    Person person;
+
     String validEmail;
     String validPassword;
+    String viewName;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         validEmail = "ali.hassan@sgbookcollectors.com";
         validPassword = "MMMnnn34";
+        person = new Librarian("Ali Hassan", "ali_hassan", validEmail, validPassword);
     }
 
     @Test
@@ -27,5 +49,16 @@ class LoginControllerTest {
         assertEquals("login", loginController.showLogin());
     }
 
+    @Test
+    void test_loginSuccess() throws UnsuccessfulLoginException {
+        // Arrange
+        when(loginService.login(validEmail, validPassword)).thenReturn(person);
+
+        // Act
+        viewName = loginController.login(validEmail, validPassword, httpSession, model);
+
+        // Assert
+        assertEquals("redirect:/home", viewName);
+    }
 
 }
